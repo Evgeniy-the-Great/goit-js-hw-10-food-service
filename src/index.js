feafdde62d57b6fd49ae0a9f0c8462caa50bbb88
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-08-31 15:28:44 +0300

    do task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import menuTpl from './templates/menu-card.hbs'
 import './styles.css';
 import menu from './menu.json'
+import LS from './LS'
 
 
 const listRef = document.querySelector('.js-menu');
@@ -8,13 +9,31 @@ const checkboxRef = document.querySelector('#theme-switch-toggle');
 
 listRef.insertAdjacentHTML('beforeend', menuTpl(menu));
 checkboxRef.addEventListener('change', onInputChange);
-document.body.classList.add('light-theme');
+// document.body.classList.add('light-theme');
 
-function onInputChange(event) {
-  if (document.body.classList.contains('dark-theme')) {
-    return document.body.classList.remove('dark-theme');
-    
-  }
-  document.body.classList.add('dark-theme');
-  localStorage.setItem('settings', settings);
+
+function onInputChange(e) {
+  const input = e.target;
+  const isChanged = input.checked;
+  
+
+  document.body.classList.toggle('dark-theme');
+  LS.save('isChecked', isChanged);
+
+  // const isInputChecked = LS.load('isChecked');
+  // console.log(isInputChecked);
+  // if (isInputChecked) {
+  //   document.body.classList.add('dark-theme');
+  //   checkboxRef.checked = isInputChecked;
+  // }
 }
+const changeCheck = () => {
+  const isInputChecked = LS.load('isChecked');
+  console.log(isInputChecked);
+  if (isInputChecked) {
+    document.body.classList.add('dark-theme');
+    checkboxRef.checked = isInputChecked;
+  }
+}
+
+changeCheck();
